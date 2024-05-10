6742dfff80a655d9c8e5bfd13cd681e2b7de1ff3
Eduard Almar Oliva <eduard.almar@local.eurecat.org>
2024-05-10 13:56:45 +0200

    added simple connectivity with daq

--- sketches/baseline/usb_simple_ascii.py
+++ sketches/baseline/usb_simple_ascii.py
@@ -1,12 +1,11 @@
 import serial
 import keyboard
 import time
-import sys
 
 #Press key 'x' to EXIT
 #since serDataq.readline waits until a line is received, low sample rate makes the program slow in response to key stroke
 
-CONST_SER_PORT = 'COM4'   #get the com port from device manger and enter it here
+CONST_SER_PORT = 'COM3'   #get the com port from device manger and enter it here
 
 serDataq = serial.Serial(CONST_SER_PORT)
 
@@ -17,12 +16,12 @@ serDataq.write(b"slist 0 0\r")   #scan list position 0 channel 0 thru channel 7
 serDataq.write(b"slist 1 1\r")
 serDataq.write(b"slist 2 2\r")
 serDataq.write(b"slist 3 3\r")
-serDataq.write(b"slist 4 4\r")
-serDataq.write(b"slist 5 5\r")
-serDataq.write(b"slist 6 6\r")
-serDataq.write(b"slist 7 7\r")
+# serDataq.write(b"slist 4 4\r")
+# serDataq.write(b"slist 5 5\r")
+# serDataq.write(b"slist 6 6\r")
+# serDataq.write(b"slist 7 7\r")
 serDataq.write(b"srate 6000\r") 
-serDataq.write(b"dec 500\r") 
+serDataq.write(b"dec 5\r") 
 serDataq.write(b"deca 3\r") 
 time.sleep(1)  
 serDataq.read_all()              #flush all command responses
@@ -40,7 +39,7 @@ while True:
             i= serDataq.inWaiting()
             if i>0:
                 line = serDataq.readline().decode("utf-8")
-                print(line)
+                print(line, end="\r")
             pass
     except:
         pass
--- sketches/usb/example_usb.py
+++ sketches/usb/example_usb.py
@@ -3,10 +3,18 @@ import os
 import datetime
 import daq_connectivity as daq 
 
-path_to_save = "C:\\Users\\eduard.almar\\OneDrive - EURECAT\\Escritorio\\proyectos\\7. Suricata\\repo\\logger"
+test_mode = 'simple'
+output_mode = 'binary'
+
+
+path_to_save = "C:\\Users\\eduard.almar\\OneDrive - EURECAT\\Escritorio\\proyectos\\7. Suricata\\repo\\daq_connectivity\\logger"
 date_name = str(datetime.datetime.now().date()) + '_' + str(datetime.datetime.now().time()).replace(':', '.')
 file_path = os.path.join(path_to_save, f'{date_name}.txt')
 
-usb = daq.USB_connection(dec=512, srate=11718)
+if test_mode == 'advanced':
+    usb = daq.USB_connection(dec=512, srate=11718)
+    usb.collect_data(file_path)
 
-usb.collect_data_with_interrupt(file_path)
+elif test_mode == 'simple':
+    usb = daq.USB_connection_simple(dec=100,deca=1, srate=6000, )
+    usb.collect_data_simple(file_path, output_mode='binary' ,binary_method=1)
