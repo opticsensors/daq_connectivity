97a6eb033ba56b6f4cb54d30d6f3933ccebf00dc
opticsensors <edu12due@gmail.com>
2024-05-16 12:50:29 +0200

    added notes and socket connection

--- rpi/server_socket.py
+++ rpi/server_socket.py
@@ -4,7 +4,7 @@ import socket
 soc = socket.socket()
 soc.bind(('192.168.1.43',8080))
 soc.listen(1)
-filename = './results/2024-05-16_11.12.06.035455.txt'
+filename = './results/2024-05-16_11.12.06.035455.csv'
 
 print('waiting for connection...')
 with soc:
