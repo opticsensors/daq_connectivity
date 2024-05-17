0152eb7b29d894027eb34591a5aa25ca8a356df9
Eduard Almar Oliva <eduard.almar@local.eurecat.org>
2024-05-17 08:19:44 +0200

    corrected client

--- pc/client_socket.py
+++ pc/client_socket.py
@@ -2,8 +2,8 @@
 import socket
 
 soc = socket.socket()
-soc.connect(('localhost',8080))
-savefilename = './results/2024-05-16_11.12.06.035455.txt'
+soc.connect(('192.168.1.43',8080))
+savefilename = './results/2024-05-16_11.12.06.035455.csv'
 with soc,open(savefilename,'wb') as file:
     while True:
         recvfile = soc.recv(4096)
